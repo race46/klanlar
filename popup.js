fa3766060f65ead687a547b0a67b4d69dc616db2
race46 <muhammedyildiz.maras@gmail.com>
2025-09-11 02:51:47 +0300

    green

--- popup.js
+++ popup.js
@@ -23,10 +23,12 @@ document.addEventListener('DOMContentLoaded', function () {
         });
       });
 
-      document.getElementById('pre-bot').addEventListener('click', function () {
+      document.getElementById('pre-bot').addEventListener('click', function (ev) {
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
           chrome.tabs.sendMessage(tabs[0].id, { action: 'startPreBot' });
         });
+
+        ev.target.style.backgroundColor = 'green'
       });
 
       document.getElementById('stop').addEventListener('click', function () {
